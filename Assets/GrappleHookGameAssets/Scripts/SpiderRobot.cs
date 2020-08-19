b8ea235919a2bcdcb12017c6a2584c78e93095a0
unknown <Reetu Kontio>
2020-08-19 15:57:08 +0300

    shake working

--- Assets/GrappleHookGameAssets/Scripts/SpiderRobot.cs
+++ Assets/GrappleHookGameAssets/Scripts/SpiderRobot.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 
 public class SpiderRobot : MonoBehaviour
 {
+    public CameraBehavior cameraBehavior;
+
     public float speed;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * speed);
+    }
+
+    public void LegImpact()
+    {
+        StartCoroutine(cameraBehavior.Shake(1.0f, 1000000f, transform.position));
     }
 }
